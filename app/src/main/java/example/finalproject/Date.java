5d421a0cfaa0b9f1c15008786ab6e0d32f52a85a
taltsr <ttaltesler@gmail.com>
2017-02-02 23:41:23 +0200

    Update

--- app/src/main/java/example/finalproject/Date.java
+++ app/src/main/java/example/finalproject/Date.java
@@ -4,28 +4,36 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import org.w3c.dom.Text;
+
 public class Date extends AppCompatActivity {
     DatePicker d;
-    TextView tv;
+    TextView tv,tv3;
     TimePicker time;
+    NumberPicker numberPicker;
     int day,month,year,hour,min;
-    AlertDialog.Builder adbd, adbt;
-    AlertDialog add, adt;
+    AlertDialog.Builder adbd, adbt, adbc;
+    AlertDialog add, adt, adc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
         d = new DatePicker(this);
+        numberPicker = new NumberPicker(this);
         time = new TimePicker(this);
         tv = (TextView)findViewById(R.id.tvdate);
+        tv3 = (TextView)findViewById(R.id.tvclass);
         //Date alert dialog
         adbd = new AlertDialog.Builder(this);
         adbd.setView(d);
@@ -49,11 +57,24 @@ public class Date extends AppCompatActivity {
             }
         });
         adt = adbt.create();
+        //Class alert dialog
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(4);
+        numberPicker.setDisplayedValues(new String[] {"English", "Math", "Biotechnology", "History", "Literature"});
+        adbc = new AlertDialog.Builder(this);
+        adbc.setView(numberPicker);
+        adbc.setPositiveButton("Proceed",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+            }
+        });
+        adc = adbc.create();
+
     }
 
-    public void setDate(View view) {
-        add.show();
-    }
+    public void setDate(View view) {add.show();}
 
     public void setTime(View view) { adt.show();}
+
+    public void setClass(View view) { adc.show();}
 }
